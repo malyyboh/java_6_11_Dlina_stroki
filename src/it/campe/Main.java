5a8f82626c355c9655e43a6db0377fb778e2efb9
Ksenx73 <hello@itcampe.com>
2021-10-14 17:02:29 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,13 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String emptyString = "";
+
+        //напишите тут ваш код
+        System.out.println(emptyString);
+        System.out.println("Gomu Gomu no Bazooka!");
+        System.out.println((emptyString + 2 + 2 + "22"));
+    }
+}
